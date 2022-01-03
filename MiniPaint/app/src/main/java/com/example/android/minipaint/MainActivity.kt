1cb7e69d23b40b93cb7879f27963565ff9fd4ea0
Hector122 <hector122@gmail.com>
2022-01-03 12:29:12 -0400

    Add drawing with canvas

---
+++ MiniPaint/app/src/main/java/com/example/android/minipaint/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.android.minipaint
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        val myCanvasView = MyCanvasView(this)
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        
+        //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        //            window.setDecorFitsSystemWindows(false)
+        //        } else {
+        //            window.decorView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        //        }
+        
+        setContentView(myCanvasView)
+        
+    }
+}
